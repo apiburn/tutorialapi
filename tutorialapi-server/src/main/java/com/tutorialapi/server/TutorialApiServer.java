4e2b598a3a46c18a233383c5b5f3cb1328114df0
apiburn <mday@apiburn.com>
2021-11-20 14:21:19 -0600

    sorted pom plugins, added logging

--- tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
+++ tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
@@ -1,12 +1,16 @@
 package com.tutorialapi.server;
 
 import org.eclipse.jetty.server.HttpConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.eclipse.jetty.http.HttpScheme.HTTPS;
 
 public class TutorialApiServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TutorialApiServer.class);
+
     public static void main(String... args) {
-        System.out.println("Hello World!");
+        LOGGER.info("Hello World!");
 
         HttpConfiguration httpsConfiguration = new HttpConfiguration();
         httpsConfiguration.setSecureScheme(HTTPS.asString());
