8787cfa7a33129494cf20ca4218c325426d8cad9
apiburn <mday@apiburn.com>
2021-11-20 19:31:45 -0600

    update server to run jetty

--- tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
+++ tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
@@ -1,18 +1,43 @@
 package com.tutorialapi.server;
 
-import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.eclipse.jetty.http.HttpScheme.HTTPS;
+import static org.eclipse.jetty.http.HttpVersion.HTTP_1_1;
 
 public class TutorialApiServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(TutorialApiServer.class);
 
-    public static void main(String... args) {
-        LOGGER.info("Hello World!");
-
+    public static void main(String... args) throws Exception {
         HttpConfiguration httpsConfiguration = new HttpConfiguration();
         httpsConfiguration.setSecureScheme(HTTPS.asString());
+        httpsConfiguration.setSecurePort(8443);
+        httpsConfiguration.addCustomizer(new SecureRequestCustomizer());
+        httpsConfiguration.setSendServerVersion(false);
+
+        HttpConnectionFactory httpsConnectionFactory = new HttpConnectionFactory(httpsConfiguration);
+
+        SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
+        sslContextFactory.setKeyStorePath("tutorialapi-server/src/main/resources/certs/tutorialapi.p12");
+        sslContextFactory.setKeyStoreType("PKCS12");
+        sslContextFactory.setKeyStorePassword("changeit");
+        sslContextFactory.setKeyManagerPassword("changeit");
+        sslContextFactory.setTrustAll(true);
+
+        SslConnectionFactory sslConnectionFactory = new SslConnectionFactory(sslContextFactory, HTTP_1_1.asString());
+
+        Server server = new Server();
+
+        ServerConnector httpsConnector = new ServerConnector(server, sslConnectionFactory, httpsConnectionFactory);
+        httpsConnector.setName("secure");
+        httpsConnector.setPort(httpsConfiguration.getSecurePort());
+
+        server.addConnector(httpsConnector);
+
+        server.start();
+        server.join();
     }
 }
